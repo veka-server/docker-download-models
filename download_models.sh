8562efda96c72c03664168fdde29ff048ee80d74
Veka <veka610@gmail.com>
2025-11-25 17:09:18 +0100

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -27,18 +27,18 @@ download_urls() {
 
 # UNET
 download_urls /models/unet \
-  "https://huggingface.co/city96/FLUX.1-dev-gguf/resolve/main/flux1-dev-Q8_0.gguf?download=true" "flux1-dev-Q8_0.gguf" \
-  "https://huggingface.co/city96/FLUX.1-schnell-gguf/resolve/main/flux1-schnell-Q8_0.gguf?download=true" "flux1-schnell-Q8_0.gguf" \
-  "https://huggingface.co/city96/HiDream-I1-Full-gguf/resolve/main/hidream-i1-full-Q8_0.gguf?download=true" "hidream-i1-full-Q8_0.gguf" \
-  "https://huggingface.co/Kijai/HunyuanVideo_comfy/resolve/main/hunyuan_video_I2V-Q8_0.gguf?download=true" "hunyuan_video_I2V-Q8_0.gguf" \
-  "https://huggingface.co/Aitrepreneur/FLX/resolve/main/Wan2.1-VACE-14B-Q4_K_S.gguf?download=true" "wan2.1-VACE-14B-Q4_K_S.gguf" \
-  "https://huggingface.co/silveroxides/Chroma-GGUF/resolve/main/chroma-unlocked-v31/chroma-unlocked-v31-Q5_0.gguf" "chroma-unlocked-v31-Q5_0.gguf" \
-  "https://huggingface.co/bullerwins/FLUX.1-Kontext-dev-GGUF/resolve/main/flux1-kontext-dev-Q6_K.gguf" "flux1-kontext-dev-Q6_K.gguf" \
+#  "https://huggingface.co/city96/FLUX.1-dev-gguf/resolve/main/flux1-dev-Q8_0.gguf?download=true" "flux1-dev-Q8_0.gguf" \
+#  "https://huggingface.co/city96/FLUX.1-schnell-gguf/resolve/main/flux1-schnell-Q8_0.gguf?download=true" "flux1-schnell-Q8_0.gguf" \
+#  "https://huggingface.co/city96/HiDream-I1-Full-gguf/resolve/main/hidream-i1-full-Q8_0.gguf?download=true" "hidream-i1-full-Q8_0.gguf" \
+#  "https://huggingface.co/Kijai/HunyuanVideo_comfy/resolve/main/hunyuan_video_I2V-Q8_0.gguf?download=true" "hunyuan_video_I2V-Q8_0.gguf" \
+#  "https://huggingface.co/Aitrepreneur/FLX/resolve/main/Wan2.1-VACE-14B-Q4_K_S.gguf?download=true" "wan2.1-VACE-14B-Q4_K_S.gguf" \
+#  "https://huggingface.co/silveroxides/Chroma-GGUF/resolve/main/chroma-unlocked-v31/chroma-unlocked-v31-Q5_0.gguf" "chroma-unlocked-v31-Q5_0.gguf" \
+#  "https://huggingface.co/bullerwins/FLUX.1-Kontext-dev-GGUF/resolve/main/flux1-kontext-dev-Q6_K.gguf" "flux1-kontext-dev-Q6_K.gguf" \
   "https://huggingface.co/QuantStack/Wan2.2-T2V-A14B-GGUF/resolve/main/LowNoise/Wan2.2-T2V-A14B-LowNoise-Q4_K_M.gguf" "Wan2.2-T2V-A14B-LowNoise-Q4_K_M.gguf" \
   "https://huggingface.co/QuantStack/Wan2.2-T2V-A14B-GGUF/resolve/main/HighNoise/Wan2.2-T2V-A14B-HighNoise-Q4_K_M.gguf" "Wan2.2-T2V-A14B-HighNoise-Q4_K_M.gguf" \
   "https://huggingface.co/QuantStack/Wan2.2-I2V-A14B-GGUF/resolve/main/HighNoise/Wan2.2-I2V-A14B-HighNoise-Q4_K_M.gguf" "Wan2.2-I2V-A14B-HighNoise-Q4_K_M.gguf" \
   "https://huggingface.co/QuantStack/Wan2.2-I2V-A14B-GGUF/resolve/main/LowNoise/Wan2.2-I2V-A14B-LowNoise-Q4_K_M.gguf" "Wan2.2-I2V-A14B-LowNoise-Q4_K_M.gguf" \
-  "https://huggingface.co/QuantStack/Qwen-Image-Edit-2509-GGUF/resolve/main/Qwen-Image-Edit-2509-Q8_0.gguf" "Qwen-Image-Edit-2509-Q8_0.gguf"
+#  "https://huggingface.co/QuantStack/Qwen-Image-Edit-2509-GGUF/resolve/main/Qwen-Image-Edit-2509-Q8_0.gguf" "Qwen-Image-Edit-2509-Q8_0.gguf"
 
 # CLIP
 download_urls /models/clip \
@@ -60,15 +60,15 @@ download_urls /models/text_encoders \
 download_urls /models/vae \
   "https://huggingface.co/foxmail/flux_vae/resolve/main/ae.safetensors?download=true" "ae_flux.safetensors" \
   "https://huggingface.co/Comfy-Org/HunyuanVideo_repackaged/resolve/main/split_files/vae/hunyuan_video_vae_bf16.safetensors?download=true" "hv_vae_bf16.safetensors" \
-  "https://huggingface.co/Comfy-Org/HiDream-I1_ComfyUI/resolve/main/split_files/vae/ae.safetensors?download=true" "ae_hidream.safetensors" \
+#  "https://huggingface.co/Comfy-Org/HiDream-I1_ComfyUI/resolve/main/split_files/vae/ae.safetensors?download=true" "ae_hidream.safetensors" \
   "https://huggingface.co/Aitrepreneur/FLX/resolve/main/wan_2.1_vae.safetensors?download=true" "wan_2.1_vae.safetensors" \
   "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/vae/wan2.2_vae.safetensors" "wan2.2_vae.safetensors" \
   "https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/vae/qwen_image_vae.safetensors" "qwen_image_vae.safetensors"
 
 # DIFFUSION MODELS
 download_urls /models/diffusion_models \
-  "https://huggingface.co/Kijai/HunyuanVideo_comfy/resolve/main/hunyuan_video_720_cfgdistill_fp8_e4m3fn.safetensors?download=true" "hv_720_cfgdistill_fp8.safetensors" \
-  "https://huggingface.co/Comfy-Org/HunyuanVideo_repackaged/resolve/main/split_files/diffusion_models/hunyuan_video_v2_replace_image_to_video_720p_bf16.safetensors?download=true" "hv_v2_720p_bf16.safetensors"
+#  "https://huggingface.co/Kijai/HunyuanVideo_comfy/resolve/main/hunyuan_video_720_cfgdistill_fp8_e4m3fn.safetensors?download=true" "hv_720_cfgdistill_fp8.safetensors" \
+#  "https://huggingface.co/Comfy-Org/HunyuanVideo_repackaged/resolve/main/split_files/diffusion_models/hunyuan_video_v2_replace_image_to_video_720p_bf16.safetensors?download=true" "hv_v2_720p_bf16.safetensors"
 
 # CLIP VISION
 download_urls /models/clip_vision \
@@ -76,7 +76,7 @@ download_urls /models/clip_vision \
 
 # CHECKPOINTS
 download_urls /models/checkpoints \
-  "https://huggingface.co/Comfy-Org/ACE-Step_ComfyUI_repackaged/resolve/main/all_in_one/ace_step_v1_3.5b.safetensors?download=true" "ace_step_v1_3.5b.safetensors" \
+#  "https://huggingface.co/Comfy-Org/ACE-Step_ComfyUI_repackaged/resolve/main/all_in_one/ace_step_v1_3.5b.safetensors?download=true" "ace_step_v1_3.5b.safetensors" \
   "https://huggingface.co/boricuapab/Bagel-7B-MoT-fp8/resolve/main/ema-fp8.safetensors" "bagel7b_ema-fp8.safetensors" \
   "https://huggingface.co/cyberdelia/CyberIllustrious/resolve/main/CyberIllustrious_V4.safetensors" "CyberIllustrious_V4.safetensors" \
   "https://huggingface.co/Zacckory10/biglove-photo2-sdxl/resolve/main/bigLove_photo2.safetensors" "bigLove_photo2.safetensors" \
